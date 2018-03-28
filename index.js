3c04f9908d18592fa14b0501106c4b2e103227ba
LuisTessaro <lh.pedrosot@hotmail.com>
2018-03-28 17:12:15 -0300

    Commit libs n code

---
+++ index.js
@@ -0,0 +1,19 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.on('message', msg => {
+
+    if(msg.content === 'fe')
+    {
+        msg.reply('ELE É MUITO G0Y');
+    }
+    
+});
+
+client.login('NDI4NjMzNjA2MjA3MDQ1NjMy.DZ178Q.SrZwICZ_BoR2NcwliKPf294Mxk4');
+
+
